dbd73effcbca03d713ce3b179f4b68a45bc724fd
lbilyk <38861662+lbilyk@users.noreply.github.com>
2018-05-05 19:43:54 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+#This code uses opencv to initialize the webcam and capture images
+#images are converted to grayscale and saved to a local file
+#edited by Lu Bilyk
+
+import numpy as np
+import cv2
+
+capture = cv2.VideoCapture(0)
+
+from pkg_resources import parse_version
+OPCV3 = parse_version(cv2.__version__) >= parse_version('3')
+
+def capPropId(prop):
+  return getattr(cv2 if OPCV3 else cv2.cv,
+    ("" if OPCV3 else "CV_") + "CAP_PROP_" + prop)
+
+capture.set(capPropId("FRAME_WIDTH"), 640)
+capture.set(capPropId("FRAME_HEIGHT"), 480)
+
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = capture.read()
+
+    if ret == True:
+        # Our operations on the frame come here
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.imwrite('gray_image.png', gray)
+        cv2.putText(frame, "MASTER PROGRAMMER!!!!", (150, 440), cv2.FONT_HERSHEY_DUPLEX, .9, (50, 225, 50))
+        # Display the resulting frame
+        cv2.imshow('frame',gray)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+# When everything done, release the capture
+capture.release()
+cv2.destroyAllWindows()
